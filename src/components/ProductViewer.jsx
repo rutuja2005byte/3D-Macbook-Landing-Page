b60a905d8e1928b63991b678ca6abe92d5f14ea1
Rutuja <rutujadarade2005@gmail.com>
2026-06-13 19:14:20 +0530

    Render MacBook model instead of placeholder Box
    Replace the simple Box placeholder with the MacBook 16" model in the 3D Canvas: add an import for the MacBook model, insert an ambientLight, and mount <MacBookModel14> with scale and position adjustments. Remove the Box that used material-color. This updates the viewer to display the actual product model (OrbitControls remains in place).

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -2,6 +2,7 @@ import useMacBookStore from "../store";
 import clsx from "clsx";
 import { Canvas } from "@react-three/fiber";
 import { Box, OrbitControls } from "@react-three/drei";
+import MacBookModel14 from "./models/Macbook-16";
 
 const ProductViewer = () => {
     const {color, scale, setColor, setScale} = useMacBookStore();
@@ -41,8 +42,8 @@ const ProductViewer = () => {
                 </div>
             </div>
             <Canvas id="canvas" camera={{ position: [0,2,5], fov: 50, near:0.1, far:100}}>
-                <Box position={[0, 0, 0]} scale={10* scale} material-color={color} />
-
+                <ambientLight intensity={1} />
+                <MacBookModel14 scale={0.06} position={[0, 0, 0]} />
                 <OrbitControls enableZoom={false} />
             </Canvas>
         </section>
