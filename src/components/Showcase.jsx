85ab15614f32bc0293691512acc72e4f250135b7
Rutuja <rutujadarade2005@gmail.com>
2026-06-16 13:38:45 +0530

    Update Showcase.jsx

--- src/components/Showcase.jsx
+++ src/components/Showcase.jsx
@@ -1,6 +1,13 @@
 const Showcase = () => {
     return (
-        <div>Showcase</div>
+       <section id="showacase">
+            <div className="media">
+                <video src="/videos/game.mp4" loop muted autoplay playsInline />
+                <div className="mask">
+                    <img src="/mask-logo.svg" />
+                </div>
+            </div>
+       </section>
     )
 }
 
